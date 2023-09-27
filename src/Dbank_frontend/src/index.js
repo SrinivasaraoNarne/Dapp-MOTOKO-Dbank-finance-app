599355f6479ea22fe63efc647eecd341147bb5a2
srinivasarao <n.srini.5108887@gmail.com>
2023-09-27 10:26:22 +0530

    initial commit

---
+++ src/Dbank_frontend/src/index.js
@@ -0,0 +1,36 @@
+import { Dbank_backend } from "../../declarations/Dbank_backend";
+
+window.addEventListener("load", async function(){
+  update();
+});
+
+document.querySelector("form").addEventListener("submit", async function(event){
+  event.preventDefault();
+
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true);
+
+  if (document.getElementById("input-amount").value.lenght != 0){
+    await Dbank_backend.topUp(inputAmount);
+  };
+  if (document.getElementById("withdrawal-amount").value.lenght != 0){
+    await Dbank_backend.withdraw(outputAmount);
+  };
+
+  await Dbank_backend.compound();
+
+  update();
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+  button.removeAttribute("disabled");
+});
+
+async function update(){
+  const currentAmount = await Dbank_backend.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100)/100;
+};
